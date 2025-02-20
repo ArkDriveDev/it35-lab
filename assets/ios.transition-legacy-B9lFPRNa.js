f53fa4766759e5b4138a07dc8faa2e27bb4f7421
Arky <20231736nbsc.edu.ph>
2025-02-20 15:16:58 +0800

    Updates

--- assets/ios.transition-legacy-B9lFPRNa.js
+++ assets/ios.transition-legacy-B9lFPRNa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BDi4V8WJ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DN784P2H.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
