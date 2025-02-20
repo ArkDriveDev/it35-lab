4387846666767234ebbaa0f6b9f0bf9304124a66
Arky <20231736nbsc.edu.ph>
2025-02-20 14:58:45 +0800

    Updates

--- assets/swipe-back-legacy-J_MdEtVG.js
+++ assets/swipe-back-legacy-J_MdEtVG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CFsnJSma.js","./index3-legacy-Bbb2ZDCF.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CC39h36o.js","./index3-legacy-FRX0Wiok.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
