0d46adfe89b8632b71d57d34c395c01c3b0a9ec3
Arky <20231736@nbsc.edu.ph>
2025-03-27 21:15:25 +0800

    added proper userinput for email and password and other buttons

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,10 +7,13 @@ import {
       IonToolbar, 
       useIonRouter,
       IonItem,
-      IonInput
+      IonInput,
+      IonInputPasswordToggle
   } from '@ionic/react';
-  
+  import { useState } from 'react';
   const Login: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const navigation = useIonRouter();
 
     const doLogin = () => {
@@ -27,18 +30,46 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent className='ion-padding'>
-        <IonItem>
-        <IonInput label="Text input" placeholder="Username:"></IonInput>
-        </IonItem>
-        <IonItem>
-        <IonInput label="Text input" placeholder="Email:"></IonInput>
-      </IonItem>
-            <IonButton onClick={() => doLogin()} expand="full">
-                Login
-            </IonButton>
-            <IonButton onClick={() => doregister()} expand="full">
-               register
-            </IonButton>
+        <div style={{
+          display: 'flex',
+          flexDirection:'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop:'5%'
+        }}>
+         
+          <h1 style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>USER LOGIN</h1>
+          <IonInput
+            label="Email" 
+            labelPlacement="floating" 
+            fill="outline"
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onIonChange={e => setEmail(e.detail.value!)}
+          />
+          <IonInput style={{ marginTop:'10px' }}      
+            fill="outline"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onIonChange={e => setPassword(e.detail.value!)}
+          >
+            <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+          </IonInput>
+        </div>
+        <IonButton onClick={doLogin} expand="full" shape='round'>
+          Login
+        </IonButton>
+
+        <IonButton routerLink="/it35-lab/register" expand="full" fill="clear" shape='round'>
+          Don't have an account? Register here
+        </IonButton>
+
         </IonContent>
       </IonPage>
     );
