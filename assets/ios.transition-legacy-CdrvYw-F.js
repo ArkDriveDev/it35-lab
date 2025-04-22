9087b11d15e32adbc000613325b633d8128cc87f
JethroBalaga <balagajet@gmail.com>
2025-04-22 18:06:24 +0800

    Updates

--- assets/ios.transition-legacy-CdrvYw-F.js
+++ assets/ios.transition-legacy-CdrvYw-F.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bg_Mguf2.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BxjT8rHW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
