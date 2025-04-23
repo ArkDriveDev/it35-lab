a7a8bfbb808c59d5b5ca3143ec8e3f543234d21b
Angelodalde00 <lesterdalde27@gmail.com>
2025-04-23 17:29:36 +0800

    Updates

--- assets/ios.transition-legacy-eUyAyAnK.js
+++ assets/ios.transition-legacy-eUyAyAnK.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bc_CW4U3.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Dd7Dme-b.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
