1bc00aa2224f6828ebcb7a42b5a1bc191128d1b4
Angelodalde00 <lesterdalde27@gmail.com>
2025-04-23 17:35:46 +0800

    Updates

--- assets/swipe-back-legacy-B7Mx4dpT.js
+++ assets/swipe-back-legacy-B7Mx4dpT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dd7Dme-b.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CgyVr45h.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
